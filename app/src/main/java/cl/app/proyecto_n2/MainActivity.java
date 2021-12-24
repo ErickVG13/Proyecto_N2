e367cebda20a7cb4205f447bdec5edd3a64f0e51
Erick <erick.estay@virginiogomez.cl>
2021-12-24 06:12:04 -0300

    Version 3.5

--- app/src/main/java/cl/app/proyecto_n2/MainActivity.java
+++ app/src/main/java/cl/app/proyecto_n2/MainActivity.java
@@ -3,6 +3,8 @@ package cl.app.proyecto_n2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -13,11 +15,19 @@ public class MainActivity extends AppCompatActivity {
     private Spinner sp_para;
     private CalendarView cale_pas;
     private TextView txt_hora;
+    private Button bt_reserva_1;
+
+    private String De = "";
+    private String Para = "";
+    private String Calendario = "";
+    private String Hora = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bt_reserva_1 = (Button)  findViewById(R.id.bt_reserva_1);
 
         sp_de = (Spinner)  findViewById(R.id.sp_de);
         sp_para = (Spinner)  findViewById(R.id.sp_para);
@@ -25,6 +35,15 @@ public class MainActivity extends AppCompatActivity {
         cale_pas = (CalendarView)  findViewById(R.id.cale_pas);
 
         txt_hora = (TextView)  findViewById(R.id.txt_hora);
+
+        bt_reserva_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+
+            }
+        });
 
     }
 }
